432458358fa53fdab359710e262f30fdeaa64e5f
Duc Dinh <holahu9@gmail.com>
2020-11-25 00:11:03 -0500

    final fix bug

--- asset/script.js
+++ asset/script.js
@@ -85,7 +85,7 @@ function generatePassword(){
 // write password to the password input
   function writePassword() {
     var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+    
     passwordText.value = password;
   }
 
